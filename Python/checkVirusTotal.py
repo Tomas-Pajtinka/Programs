0ae8fae614c677d3a13d0aa4a73ca4a69d7901a1
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-10-26 11:50:47 +0200

    Update checkVirusTotal.py

--- Python/checkVirusTotal.py
+++ Python/checkVirusTotal.py
@@ -1,12 +1,16 @@
-import argparse, os, vt, hashlib, datetime 
+import argparse, os, vt, hashlib, datetime, time
 from calendar import monthrange
 
 
 min_first_submit = round(datetime.datetime.strptime(datetime.date.today().strftime("%d/%m/%Y"), "%d/%m/%Y").timestamp()) #get today UNIX timestamp
-extensions = [".exe", ".dll", ".sys", ".pdf", ".xls", ".doc", ".xlsm", ".docm"]
+min_last_analysis = 0
+extensions = [".exe", ".dll", ".sys"] #, ".pdf", ".xls", ".doc", ".xlsm", ".docm"]
 ignore_extensions = False
 client = None
 
+def logging(log_msg):
+    with open("vt_log.txt", "a") as log:
+        log.write(log_msg + "\n")
 
 def getFiles(input):
     out = []
@@ -41,6 +45,12 @@ def printSus(file, analysis):
 def printTime(file, analysis):
     print("New file >> " + file + "\t" +  datetime.datetime.utcfromtimestamp(analysis['attributes']['first_submission_date']).strftime("%d/%m/%Y"))
 
+def submitFile(file, hash):
+    with open(file, "rb") as f:
+        analysis = client.scan_file(f, wait_for_completion=True)
+
+    time.sleep(15)
+    return client.get_object("/files/" + hash)
 
 def getFileInfo(file):
     hash = sha256sum(file)
@@ -49,17 +59,28 @@ def getFileInfo(file):
     except Exception as e:
         if e.code == "NotFoundError":
             print("Not found >> "+ file)
+            vtInfo = submitFile(file, hash)
         else:
             print("Error >> " + file )
             print(e)
-        return
+            return
     analysis = vtInfo.to_dict()
 
     if analysis['attributes']['last_analysis_stats']['malicious'] > 0 or analysis['attributes']['last_analysis_stats']['suspicious'] > 0 or analysis['attributes']['last_analysis_stats']['timeout'] > 25 or analysis['attributes']['last_analysis_stats']['confirmed-timeout'] > 25:
         printSus(file, analysis)
         return
+        
     if analysis['attributes']['first_submission_date'] > min_first_submit:
         printTime(file, analysis)
+        return
+
+    if analysis['attributes']['last_analysis_date'] < min_last_analysis:
+        vtInfo = submitFile(file, hash)
+        analysis = vtInfo.to_dict()
+        if analysis['attributes']['last_analysis_stats']['malicious'] > 0 or analysis['attributes']['last_analysis_stats']['suspicious'] > 0 or analysis['attributes']['last_analysis_stats']['timeout'] > 25 or analysis['attributes']['last_analysis_stats']['confirmed-timeout'] > 25:
+            printSus(file, analysis)
+        return
+
     return
 
     
@@ -70,41 +91,62 @@ def getFilesInfo(input):
         files = [input]
     
     for file in files:
+        logging(file)
         if globals()['ignore_extensions'] == False:  #if ignore_extensions switch was not used
             for extension in globals()['extensions']:
                 if file.endswith(extension):
                     getFileInfo(file)
+                    time.sleep(16) #to do not exceded limit quota for free api key
         else:
             getFileInfo(file)
-    
+            time.sleep(16) #to do not exceded limit quota for free api key
+          
     client.close()
 
+def listInputFile(input):
+    with open(input, "r") as file:
+        while True:
+            line = file.readline()
+            if line is None:
+                break
+            getFilesInfo(line.rstrip())
 
-def setMinFirstSubmit(days):
+
+def setMinTime(days):
     today = datetime.date.today()
     later = datetime.date(today.year, today.month, monthrange(today.year, today.month)[1]) - datetime.timedelta(int(days)) #substract specified numbers of day from current date
-    globals()['min_first_submit'] = round(datetime.datetime.strptime(later.strftime("%d/%m/%Y"), "%d/%m/%Y").timestamp())
+    return round(datetime.datetime.strptime(later.strftime("%d/%m/%Y"), "%d/%m/%Y").timestamp())
     
 
 def main():
     parser = argparse.ArgumentParser(description="Tool to scan multiple files in VirusTotal.")
+    parser.add_argument("--min_last_analysis", action="store", help="If last analysis of file is older then specified file will be analyze")
     parser.add_argument("--min_first_submit", action="store", help="If first submit of file is older then specified and is clean, than it will not be print out.")
     parser.add_argument("--ignore_extensions", action="store_true", help="Scan files with every extension.")
-    parser.add_argument("--input", action="store", required=True,  help="Path to file or directory.")
+    parser.add_argument("--input", action="store", help="Path to file or directory.")
+    parser.add_argument("--input_file", action="store",  help="Path to file containing list of files to scan.")
     parser.add_argument("--api_key", action="store", required=True,  help="VirusTotal API key.")
     args, leftovers = parser.parse_known_args()
 
+    globals()['client'] = vt.Client(args.api_key)
+
     if args.min_first_submit is not None:
-        setMinFirstSubmit(args.min_first_submit)
-    print("Submission threshold is set to " + datetime.datetime.utcfromtimestamp(globals()['min_first_submit']).strftime("%d/%m/%Y"))
+        globals()['min_first_submit'] = setMinTime(args.min_first_submit)
+    print("Minimal fisrt submission threshold is set to " + datetime.datetime.utcfromtimestamp(globals()['min_first_submit']).strftime("%d/%m/%Y"))
+
+    if args.min_last_analysis is not None:
+        globals()['min_last_analysis'] = setMinTime(args.min_last_analysis)
+    print("Minimal last analysis threshold is set to " + datetime.datetime.utcfromtimestamp(globals()['min_last_analysis']).strftime("%d/%m/%Y"))
 
     if args.ignore_extensions == True:
         globals()['ignore_extensions'] = True
 
-    input = args.input
-    globals()['client'] = vt.Client(args.api_key)
+    if args.input is not None:
+        getFilesInfo(args.input)
     
-    getFilesInfo(input)
+    if args.input_file is not None:
+        listInputFile(args.input_file)
+
 
 if __name__ == "__main__":
     main()
