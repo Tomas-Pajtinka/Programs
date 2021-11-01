615b21e5b821fe6ff909f8cb053fcf84824fa4d3
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-01 13:00:04 +0100

    Update popupTestDLL.c

--- C/Payload/Testing/popupTestDLL.c
+++ C/Payload/Testing/popupTestDLL.c
@@ -1,20 +1,30 @@
 #define WIN32_LEAN_AND_MEAN  
 #include <windows.h>
+typedef void* HANDLE;
+
+HANDLE hMutex = NULL;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call,  LPVOID lpReserved)
 {
  switch (ul_reason_for_call)
  {
  case DLL_PROCESS_ATTACH:
-    MessageBox(0,"Injection of process successfull.","Message from DLL",0);
+    hMutex = CreateMutexA(NULL,TRUE, "DllInjection");
+    if (hMutex != NULL){
+        MessageBox(0,"Injection of process successfull.","Message from DLL",0);
+    }
     break;
  case DLL_THREAD_ATTACH:
-    MessageBox(0,"Injection of thread successfull.","Message from DLL",0);
+    hMutex = CreateMutexA(NULL,TRUE, "DllInjection");
+    if (hMutex != NULL){
+        MessageBox(0,"Injection of process successfull.","Message from DLL",0);
+    }
     break;
  case DLL_THREAD_DETACH:
-   break;
+    break;
  case DLL_PROCESS_DETACH:
-  break;
+    ReleaseMutex(hMutex);
+    break;
  }
  return TRUE;
 }
