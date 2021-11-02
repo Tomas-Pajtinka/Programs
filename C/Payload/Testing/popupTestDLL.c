d9cba391dca1b5ab9517d9593ba0c4a90e734212
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-02 17:34:47 +0100

    Update popupTestDLL.c

--- C/Payload/Testing/popupTestDLL.c
+++ C/Payload/Testing/popupTestDLL.c
@@ -2,29 +2,19 @@
 #include <windows.h>
 typedef void* HANDLE;
 
-HANDLE hMutex = NULL;
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call,  LPVOID lpReserved)
 {
  switch (ul_reason_for_call)
  {
- case DLL_PROCESS_ATTACH:
-    hMutex = CreateMutexA(NULL,TRUE, "DllInjection");
-    if (hMutex != NULL){
-        MessageBox(0,"Injection of process successfull.","Message from DLL",0);
-    }
-    break;
- case DLL_THREAD_ATTACH:
-    hMutex = CreateMutexA(NULL,TRUE, "DllInjection");
-    if (hMutex != NULL){
-        MessageBox(0,"Injection of process successfull.","Message from DLL",0);
-    }
-    break;
- case DLL_THREAD_DETACH:
-    break;
- case DLL_PROCESS_DETACH:
-    ReleaseMutex(hMutex);
-    break;
- }
- return TRUE;
+   case DLL_PROCESS_ATTACH:
+      MessageBox(0,"Injection of process successfull.","Message from DLL",0);
+      break;
+   case DLL_THREAD_ATTACH:
+      break;
+   case DLL_THREAD_DETACH:
+      break;
+   case DLL_PROCESS_DETACH:
+      break;
+   }
+   return TRUE;
 }
