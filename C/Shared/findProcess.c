2b65532201078c0d3898ca3698de5a1fe64c1880
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-03 07:31:59 -0700

    Update findProcess.c

--- C/Shared/findProcess.c
+++ C/Shared/findProcess.c
@@ -10,7 +10,7 @@ int is64bit(long pid) {
     GetNativeSystemInfo(&sysinfo);
     if (sysinfo.wProcessorArchitecture != PROCESSOR_ARCHITECTURE_AMD64) 
         return 0;
-    hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pid);;
+    hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, pid);
     if (hProcess == NULL)
         return -1;
     if (! IsWow64Process(hProcess, &isWow64)) {
@@ -25,7 +25,7 @@ int is64bit(long pid) {
 }
 
 //get PID of the first 32 bit process
-int findPidOf32BitProcess(){
+int findPidOf32BitProcess(int minPid){
     PROCESSENTRY32 pe32;
     char process[MAX_PATH];
     if (!GetModuleFileNameA(NULL, process, MAX_PATH)){
@@ -39,14 +39,16 @@ int findPidOf32BitProcess(){
     }
     pe32.dwSize = sizeof(PROCESSENTRY32); //according the documentation, size must be set first
     if (Process32First(hProcessSnap, &pe32)){   //get first process
-        if (is64bit(pe32.th32ProcessID) == 0){ //check if is 32 bit
+        if (is64bit(pe32.th32ProcessID) == 0 && pe32.th32ProcessID > minPid){ //check if is 32 bit
+            printf("%s > %d\n", pe32.szExeFile, pe32.th32ProcessID);
             if (strcmp(pe32.szExeFile, processFileName) != 0){ //compare process names
                 return pe32.th32ProcessID;
             }
         }
     }
     while(Process32Next(hProcessSnap, &pe32)){
-        if (is64bit(pe32.th32ProcessID) == 0){ //check if is 32 bit
+        if (is64bit(pe32.th32ProcessID) == 0 && pe32.th32ProcessID > minPid){ //check if is 32 bit
+            printf("%s\n", pe32.szExeFile);
             if (strcmp(pe32.szExeFile, processFileName) != 0){ //compare process names
                 return pe32.th32ProcessID;
             }
