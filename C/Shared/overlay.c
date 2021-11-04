d0b7ccf56d4f705b7b7c87540940ab2bbe3aa43c
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-04 05:45:01 -0700

    Update overlay.c

--- C/Shared/overlay.c
+++ C/Shared/overlay.c
@@ -1,28 +1,13 @@
 #include <windows.h>
 #include <TlHelp32.h> 
 #include <stdio.h>
+#include "peParser.c"
 
 struct Overlay{
     int overlaySize;
     char *overlayData;
 } overlay;
 
-//get offset of overlay
-int getSizeOfPeFile(){
-    HMODULE baseAddress = GetModuleHandle(NULL);
-    IMAGE_DOS_HEADER* IDH = (IMAGE_DOS_HEADER*)baseAddress;
-    IMAGE_NT_HEADERS* INH = (IMAGE_NT_HEADERS*)((int)baseAddress + IDH->e_lfanew);
-    int size = INH->OptionalHeader.SizeOfHeaders;
-    DWORD sectionLocation = (DWORD)INH + sizeof(DWORD) /*PE signature*/ + (DWORD)(sizeof(IMAGE_FILE_HEADER)) + (DWORD)INH->FileHeader.SizeOfOptionalHeader;
-    DWORD sectionSize = (DWORD)sizeof(IMAGE_SECTION_HEADER); //40 bytes
-    DWORD importDirectoryRVA = INH->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
-    for (int i = 0; i < INH->FileHeader.NumberOfSections; i++) {    //loop through all sections and adds its sizes
-        PIMAGE_SECTION_HEADER sectionHeader = (PIMAGE_SECTION_HEADER)sectionLocation;
-        size = size + sectionHeader->SizeOfRawData;
-        sectionLocation = sectionLocation +sectionSize;
-    }
-    return size;
-}   
 
 void getOverlay(struct Overlay *overlay){
     int offsetOfOverlay = getSizeOfPeFile();
