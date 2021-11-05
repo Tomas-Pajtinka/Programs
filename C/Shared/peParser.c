4deb32e9a19847d2346d13fe1d90c86fa4c5cde5
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-05 22:18:30 +0100

    Update peParser.c

--- C/Shared/peParser.c
+++ C/Shared/peParser.c
@@ -11,45 +11,58 @@ size_t alignSize(DWORD aligment, DWORD size){
     return (size_t)(((size / aligment) + 1) * aligment);
 }
 
-int loadPeToRemoteProcess(struct Executable *executable, HANDLE hProcess){
+
+//function loads PE into memory
+void loadPeToRemoteProcess(struct Executable *executable){
     //parse DOS and PE header
     IMAGE_DOS_HEADER* IDH = (IMAGE_DOS_HEADER*)executable->executableData;
     IMAGE_NT_HEADERS* INH = (IMAGE_NT_HEADERS*)((int)executable->executableData + IDH->e_lfanew);
-
     DWORD sectionAlligmnet = INH->OptionalHeader.SectionAlignment;
     DWORD fileAlligment = INH->OptionalHeader.FileAlignment;
-
-    LPVOID remoteProcessAddress = NULL;
-    //allocate memory in remote process for header
-    LPVOID remoteProcessAddress = VirtualAllocEx(hProcess, remoteProcessAddress, alignSize(sectionAlligmnet, INH->OptionalHeader.SizeOfHeaders), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-    if (remoteProcessAddress == NULL){
-        perror("Injection failed. Could not allocate memory in remote process.\n");
-        exit(EXIT_FAILURE);
-    } 
-
-    if (WriteProcessMemory(hProcess, remoteProcessAddress, executable->executableData, alignSize(sectionAlligmnet, INH->OptionalHeader.SizeOfHeaders), NULL) == 0){
-        perror("Injection failed. Could not write to remote process.\n");
+    //allocate memory in process with enought space for image
+    LPVOID peBaseAddress = VirtualAlloc(NULL, INH->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    LPVOID nextSectionAddress = peBaseAddress;
+    if (nextSectionAddress == NULL){
+        perror("Injection failed. Could not allocate memory in process.\n");
         exit(EXIT_FAILURE);
     }
-
+    //write PE header to process
+    memcpy(nextSectionAddress, executable->executableData, INH->OptionalHeader.SizeOfHeaders);
+    //calculate next virtual address
+    nextSectionAddress = (LPVOID)((int)nextSectionAddress + alignSize(sectionAlligmnet, INH->OptionalHeader.SizeOfHeaders));//virtual address, where should next write start
+    //get section location
     DWORD sectionLocation = (DWORD)INH + sizeof(DWORD) /*PE signature*/ + (DWORD)(sizeof(IMAGE_FILE_HEADER)) + (DWORD)INH->FileHeader.SizeOfOptionalHeader;
     DWORD sectionSize = (DWORD)sizeof(IMAGE_SECTION_HEADER); //40 bytes
-    for (int i = 0; i < INH->FileHeader.NumberOfSections; i++) {    //loop through all sections 
+    //loop through all sections and write their contetn into memory
+    for (int i = 0; i < INH->FileHeader.NumberOfSections; i++) {    
         PIMAGE_SECTION_HEADER sectionHeader = (PIMAGE_SECTION_HEADER)sectionLocation;
-        //
-        remoteProcessAddress = VirtualAllocEx(hProcess, remoteProcessAddress, alignSize(sectionAlligmnet, INH->OptionalHeader.SizeOfHeaders), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-        if (remoteProcessAddress == NULL){
-            perror("Injection failed. Could not allocate memory in remote process.\n");
-            exit(EXIT_FAILURE);
-        } 
-
-        if (WriteProcessMemory(hProcess, remoteProcessAddress, executable->executableData, INH->OptionalHeader.SizeOfHeaders, NULL) == 0){
-            perror("Injection failed. Could not write to remote process.\n");
-            exit(EXIT_FAILURE);
-        }
-        //
+        memcpy(nextSectionAddress, (LPVOID)((int)executable->executableData + sectionHeader->PointerToRawData), sectionHeader->SizeOfRawData);
+        //calculate next virtual address
+        nextSectionAddress = (LPVOID)((int)nextSectionAddress + alignSize(sectionAlligmnet, sectionHeader->Misc.VirtualSize));
+        //calculate address of nexr section record
         sectionLocation = sectionLocation +sectionSize;
-    }    
+    } 
+
+    //Import resolve
+    IMAGE_DATA_DIRECTORY importsDirectory = INH->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+    PIMAGE_IMPORT_DESCRIPTOR importDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(importsDirectory.VirtualAddress + (DWORD_PTR)peBaseAddress);
+    HANDLE hLibrary = NULL;
+    LPVOID procAddr = NULL;
+    while( (void *)importDescriptor->Name != NULL){
+        hLibrary = LoadLibraryA((char *)((int)peBaseAddress + importDescriptor->Name)); //Load libray by name from Import direcotry
+        PIMAGE_THUNK_DATA thunk = NULL;
+		thunk = (PIMAGE_THUNK_DATA)((DWORD_PTR)peBaseAddress + importDescriptor->FirstThunk);//get offset of IAT entry
+        while( (void *)thunk->u1.Function != NULL){
+            procAddr = GetProcAddress( hLibrary, (LPCSTR)((DWORD_PTR)peBaseAddress + thunk->u1.Function) + 2);//get address of function
+            memcpy( thunk, &procAddr, 4);//copy address of function into IAT
+            thunk++; //next IAT entry
+        }
+        importDescriptor++; //next import descriptor
+    }
+
+
+    //TODO relocations 
+     
 }
 
 //get offset of overlay
