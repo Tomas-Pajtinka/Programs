b50410ec0b53a9121f6079cf86ef48cfc27411b1
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-07 16:04:32 +0100

    Update shellcodeInjectionOverlay.c

--- C/Techniques/Injections/shellcodeInjectionOverlay.c
+++ C/Techniques/Injections/shellcodeInjectionOverlay.c
@@ -42,11 +42,5 @@ void remoteExeInjection(){
         exit(EXIT_FAILURE);
     }
 
-    puts("Injection successfull.");
-
 }
 
-int main(int argc, char *argv[]){
-    remoteExeInjection();
-    return 0;
-}
