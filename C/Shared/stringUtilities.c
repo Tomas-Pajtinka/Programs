52c0f5884087ee37a60d0805f6f94940abe3a3e1
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-11-08 21:42:32 +0100

    Create stringUtilities.c

---
+++ C/Shared/stringUtilities.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <winternl.h>
+
+char * convertUnicodeToAscii(UNICODE_STRING unicode){
+    char *ascii = malloc(MAX_PATH);
+    int u = 0;
+    for(; u < unicode.Length/2 ; u++){
+        ascii[u] = (unicode.Buffer+u)[0];//ch[0];
+    }
+    ascii[u] = 0x00;
+    return ascii;
+}
+
+void getLastItemFromPath(char *module){
+    char *processFileName;
+    (processFileName = strrchr(module, '\\')) ? ++processFileName : (processFileName = module);   //get last item from path
+}
