3aeeb11d77316b859beaed2631a0d0cfc6f2bf68
Tomas-Pajtinka <52199126+Tomas-Pajtinka@users.noreply.github.com>
2021-12-10 04:05:46 -0800

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,17 +1,11 @@
+#include <stdio.h> 
 #include "Shared\stringUtilities.c"
 #include "Shared\peParser.c"
 #include "Shared\obfuscation.c"
-#include "Techniques/ApiResolving/pebApiResolving.c"
+#include "Techniques/Injections/remoteReflectiveExeInjectionOverlay.c"
 
 int main(int argc, char *argv[]){
-    char string[12] = "\x39\x00\x00\x07\x08\x09\x02\x36\x0a\x0b\x35";
-    decodeSimpleXor(string, 11, "testing", 7);
-    puts("A");
-    puts(string);
-    /*inicialize();
-    void (*mb)(HWND, LPCTSTR, LPCTSTR, UINT) = resolveApi("MessageBoxA", "User32.dll");
-    mb(0,"Dynamic API resolve succesfull.","Dynamic API resolve",0);
-    puts("Resolving successfull.");*/
+    remoteExeInjection();
 
     return 0;
 }
